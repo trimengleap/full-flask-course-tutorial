0d4e9455797b98b2653fea6fae63bdeb1cfdff97
trimengleap <139295261+trimengleap@users.noreply.github.com>
2024-10-07 16:53:14 +0700

    Practice session, cookies, login

--- full-flask-course-tutorial/Python/Current/firstapp/app.py
+++ full-flask-course-tutorial/Python/Current/firstapp/app.py
@@ -1,9 +1,10 @@
 import os
 import uuid
 import pandas as pd
-from flask import Flask, request, render_template, redirect, url_for, Response, send_from_directory, jsonify
+from flask import Flask, request, render_template, redirect, url_for, Response, send_from_directory, jsonify, session, make_response, flash
 
 app = Flask (__name__, template_folder = 'templates', static_folder='static', static_url_path='/')
+app.secret_key = 'SOME KEY'
 
 # The function that returns a simple "Home" message
 # @app.route('/home')
@@ -133,6 +134,59 @@ def handle_post():
           f.write(f'{greeting}, {name}')
 
      return jsonify({'message': 'Successfully written!'})
+
+
+@app.route('/set_data')
+def set_data():
+     session['name'] = 'Mike'
+     session['other'] = 'Hello world!'
+     return render_template('session.html', message = 'Session data set.')
+
+@app.route('/get_data')
+def get_data():
+     if 'name' in session.keys() and 'other' in session.keys():
+          name = session['name']
+          other = session['other']
+          return render_template('session.html', message = f'Name : {name}, Other : {other}')
+     else:
+          return render_template('session.html', message = 'Session not found')
+
+@app.route('/clear_data')
+def clear_data():
+     session.clear()
+     return render_template('session.html', message = 'Session cleared')
+
+@app.route('/set_cookie')
+def set_cookie():
+     response = make_response(render_template('session.html', message = 'Cookie set.'))
+     response.set_cookie('cookie_name', 'cookie_value')
+     return response
+     
+@app.route('/get_cookie')
+def get_cookie():
+     cookie_value = request.cookies['cookie_name']
+     return render_template('session.html', message = f'Cookie Value: {cookie_value}')
+     
+@app.route('/remove_cookie')
+def remove_cookie():
+     response = make_response(render_template('session.html', message = 'Cookie removed'))
+     response.set_cookie('cookie_name', expires=0)
+     return response
+     
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+     if request.method == 'GET':
+          return render_template('login.html')
+     elif request.method == 'POST':
+          username = request.form.get('username')
+          password = request.form.get('password')
+          if username == 'Mengleap' and password == '123':
+               flash('Successful Login!')
+               return render_template('session.html', message = '' )
+          else:
+               flash('Login Failed!')
+               return render_template('session.html', message = '')
+
 
              
 if __name__ == '__main__':
